e9192bda8d696192d2aa4569296ffbbd1d69b22f
supasit <supasit.s@hitachi-tstv.com>
2018-12-03 15:55:14 +0700

    Create Alart  JAVA 6step

--- app/src/main/java/nutt/com/hitachi_tstv/supasit/nuttfriendy/RegisterFragment.java
+++ app/src/main/java/nutt/com/hitachi_tstv/supasit/nuttfriendy/RegisterFragment.java
@@ -1,6 +1,7 @@
 package nutt.com.hitachi_tstv.supasit.nuttfriendy;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,6 +11,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toolbar;
 
 
@@ -21,6 +23,7 @@ public class RegisterFragment extends Fragment {
 
     //    Explicit
     private boolean aBoolean = true;
+    private ImageView imageView;
 
     public RegisterFragment() {
         // Required empty public constructor
@@ -33,7 +36,31 @@ public class RegisterFragment extends Fragment {
 //        Create Toolbar
         createToolbar();
 
+//        Avatar Controller
+        avatarController();
     } /// Main Method
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == getActivity().RESULT_OK) {
+            aBoolean = false;
+        } ///IF
+    }
+
+    private void avatarController() {
+        imageView = getView().findViewById(R.id.imbAvatar);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Please App and Picture"), 9);
+
+            }
+        });
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -49,7 +76,7 @@ public class RegisterFragment extends Fragment {
         MyAlert myAlert = new MyAlert(getActivity());
 
         if (aBoolean) {
-            myAlert.normalDialog("Non Choose Avatar ???","Please Choose Avatar !!!");
+            myAlert.normalDialog("Non Choose Avatar ???","Please Choose Avatar");
         }
 
     } ///CheckAndUploadVales
