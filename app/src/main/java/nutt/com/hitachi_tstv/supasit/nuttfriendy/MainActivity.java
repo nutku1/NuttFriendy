575e161fc7c7cb665e70fea6ccb4eacd6bbbfea4
supasit <supasit.s@hitachi-tstv.com>
2018-12-03 11:45:22 +0700

    Add Fragment in JAVA 2step

--- app/src/main/java/nutt/com/hitachi_tstv/supasit/nuttfriendy/MainActivity.java
+++ app/src/main/java/nutt/com/hitachi_tstv/supasit/nuttfriendy/MainActivity.java
@@ -6,8 +6,13 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //savedInstanceState เอาไว้เก็บการทำงานว่าทำถึงตอนไหน แล้วเมื่อเปิดใหม่ ให้ทำต่อ
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().add(R.id.contentMainFragment, new MainFragment()).commit();
+        }
+    } ///Main Method
+} ///Main Class
