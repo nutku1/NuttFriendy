ec6cfa2da47ded6e6b5eda2b5c5f9681a4caa9c2
supasit <supasit.s@hitachi-tstv.com>
2018-12-03 14:14:32 +0700

    Create toolbar JAVA 5step

--- app/src/main/java/nutt/com/hitachi_tstv/supasit/nuttfriendy/RegisterFragment.java
+++ app/src/main/java/nutt/com/hitachi_tstv/supasit/nuttfriendy/RegisterFragment.java
@@ -2,10 +2,12 @@ package nutt.com.hitachi_tstv.supasit.nuttfriendy;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
 
 
 /**
@@ -18,6 +20,21 @@ public class RegisterFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+    } /// Main Method
+
+    private void createToolbar() {
+        android.support.v7.widget.Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.register));
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Please fill all Blank");
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
