cb4a7cf6eae72f8d44803739f35f6e0efe5d6227
supasit <supasit.s@hitachi-tstv.com>
2018-12-03 13:53:43 +0700

    Replace Fragment in (Mainpage click newregister to Register Fragment)JAVA 4step

--- app/src/main/java/nutt/com/hitachi_tstv/supasit/nuttfriendy/MainFragment.java
+++ app/src/main/java/nutt/com/hitachi_tstv/supasit/nuttfriendy/MainFragment.java
@@ -1,11 +1,14 @@
 package nutt.com.hitachi_tstv.supasit.nuttfriendy;
 
 
+import android.icu.text.Replaceable;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -16,14 +19,41 @@ public class MainFragment extends Fragment {
 
     public MainFragment() {
         // Required empty public constructor
-    }
+    } //Constructor
 
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Register Controller
+        registerController();
+
+    } //Main Method
+
+    private void registerController() {
+        TextView textView = getView().findViewById(R.id.textRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                Replace Fragment
+                getActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
-    }
+    } //Create View
 
 }
