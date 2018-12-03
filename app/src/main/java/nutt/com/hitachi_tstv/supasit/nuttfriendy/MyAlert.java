74edd2085b71ae1267e42d8c62c373821f5a23be
supasit <supasit.s@hitachi-tstv.com>
2018-12-03 15:24:18 +0700

    Create Alart  JAVA 6step

---
+++ app/src/main/java/nutt/com/hitachi_tstv/supasit/nuttfriendy/MyAlert.java
@@ -0,0 +1,30 @@
+package nutt.com.hitachi_tstv.supasit.nuttfriendy;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+
+public class MyAlert {
+
+    private Context context;
+
+    public MyAlert(Context context) {
+        this.context = context;
+    }
+
+    public void normalDialog(String titleString, String messageString) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.ic_action_alert);
+        builder.setTitle(titleString);
+        builder.setMessage(messageString);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+} ///Main Class
--- app/src/main/java/nutt/com/hitachi_tstv/supasit/nuttfriendy/RegisterFragment.java
+++ app/src/main/java/nutt/com/hitachi_tstv/supasit/nuttfriendy/RegisterFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toolbar;
@@ -15,6 +18,9 @@ import android.widget.Toolbar;
  */
 public class RegisterFragment extends Fragment {
 
+
+    //    Explicit
+    private boolean aBoolean = true;
 
     public RegisterFragment() {
         // Required empty public constructor
@@ -29,11 +35,50 @@ public class RegisterFragment extends Fragment {
 
     } /// Main Method
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.itemUpload) {
+            CheckAndUploadValues();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void CheckAndUploadValues() {
+
+        MyAlert myAlert = new MyAlert(getActivity());
+
+        if (aBoolean) {
+            myAlert.normalDialog("Non Choose Avatar ???","Please Choose Avatar !!!");
+        }
+
+    } ///CheckAndUploadVales
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_register, menu);
+    }
+
     private void createToolbar() {
         android.support.v7.widget.Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
-        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
         ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.register));
         ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Please fill all Blank");
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                Create POP
+                getActivity().getSupportFragmentManager().popBackStack();
+
+            }
+        });
+
+        setHasOptionsMenu(true);
+
     }
 
     @Override
