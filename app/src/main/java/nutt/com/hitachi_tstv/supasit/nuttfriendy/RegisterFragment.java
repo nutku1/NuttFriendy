7fea972c4402c439df930ade2f9f7299ea577a15
supasit <supasit.s@hitachi-tstv.com>
2018-12-04 09:19:12 +0700

    Check value blank  Day2 training Android  JAVA 10step

--- app/src/main/java/nutt/com/hitachi_tstv/supasit/nuttfriendy/RegisterFragment.java
+++ app/src/main/java/nutt/com/hitachi_tstv/supasit/nuttfriendy/RegisterFragment.java
@@ -14,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toolbar;
 
@@ -92,9 +93,23 @@ public class RegisterFragment extends Fragment {
 
         MyAlert myAlert = new MyAlert(getActivity());
 
+//        Get values from Username Check
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText userEditText = getView().findViewById(R.id.edtUser);
+        EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+        String nameString = nameEditText.getText().toString().trim();
+        String userString = userEditText.getText().toString().trim();
+        String passwordString = passwordEditText.getText().toString().trim();
+
         if (aBoolean) {
-            myAlert.normalDialog("Non Choose Avatar ???","Please Choose Avatar");
+            myAlert.normalDialog("Non Choose Avatar ???", "Please Choose Avatar");
+        } else if (nameString.isEmpty()|| userString.isEmpty() || passwordString.isEmpty()) {
+            myAlert.normalDialog("Have Space","Please Fill Every Blank");
+        } else {
+
         }
+
 
     } ///CheckAndUploadVales
 
