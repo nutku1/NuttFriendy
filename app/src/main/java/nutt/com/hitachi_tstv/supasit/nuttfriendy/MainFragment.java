a6080f3951c784167a120940addf9fe214137faf
supasit <supasit.s@hitachi-tstv.com>
2018-12-04 13:43:55 +0700

    Get json Complete Day2 training Android  JAVA 13step

--- app/src/main/java/nutt/com/hitachi_tstv/supasit/nuttfriendy/MainFragment.java
+++ app/src/main/java/nutt/com/hitachi_tstv/supasit/nuttfriendy/MainFragment.java
@@ -5,9 +5,12 @@ import android.icu.text.Replaceable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -29,7 +32,51 @@ public class MainFragment extends Fragment {
 //        Register Controller
         registerController();
 
+//        Login Controller
+        loginController();
     } //Main Method
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText userEditText = getView().findViewById(R.id.edtUser);
+                EditText PasswordEditText = getView().findViewById(R.id.edtPassword);
+
+                String userString = userEditText.getText().toString().trim();
+                String passwordString = PasswordEditText.getText().toString().trim();
+
+                MyAlert myAlert = new MyAlert(getActivity());
+
+                if (userString.isEmpty() || passwordString.isEmpty()) {
+                    myAlert.normalDialog(getString(R.string.title_space), getString(R.string.message_space));
+                } else {
+
+//                    Check User and Password
+                    checkUserAndPass(userString, passwordString);
+                }
+
+            }
+        });
+    }
+
+    private void checkUserAndPass(String userString, String passwordString) {
+        MyAlert myAlert = new MyAlert(getActivity());
+        MyConstant myConstant = new MyConstant();
+        boolean b = true; //True == user false , False == user true
+
+
+        try {
+            GetAllDataThread getAllDataThread = new GetAllDataThread(getActivity());
+            getAllDataThread.execute(myConstant.getUrlGetAllUser());
+            String jsonString = getAllDataThread.get();
+            Log.d("4DecV2", "jsonString ==>" + jsonString);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     private void registerController() {
         TextView textView = getView().findViewById(R.id.textRegister);
--- app/src/main/java/nutt/com/hitachi_tstv/supasit/nuttfriendy/MyConstant.java
+++ app/src/main/java/nutt/com/hitachi_tstv/supasit/nuttfriendy/MyConstant.java
@@ -3,6 +3,11 @@ package nutt.com.hitachi_tstv.supasit.nuttfriendy;
 public class MyConstant {
     private String urlAddUser = "http://androidthai.in.th/hit/addusernuutt.php";
     private String urlPreAvata = "http://androidthai.in.th/hit/nuutt";
+    private String urlGetAllUser ="http://androidthai.in.th/hit/getallusernuutt.php";
+
+    public String getUrlGetAllUser() {
+        return urlGetAllUser;
+    }
 
     public String getUrlPreAvata() {
         return urlPreAvata;
